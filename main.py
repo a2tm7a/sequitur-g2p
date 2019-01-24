ad0242dc80c88ac49c132da14fbd4c3fdc7b1eef
Amit Manchanda <manchanda@vernacular.ai>
2019-01-24 13:02:41 +0000

    Added main file

---
+++ main.py
@@ -0,0 +1,19 @@
+import subprocess
+from subprocess import Popen, PIPE, STDOUT
+
+class G2P:
+    def __init__(self):
+        self.model = "models/models-6-en"
+
+    def train(self):
+        raise NotImplementedError("Not available right now")
+    """
+    Input should be lower case
+    """
+    def get_phoneme(self, word):
+        # os.system()
+        cmd = "python g2p.py --model " + self.model + " --word " + word
+        proc = Popen(cmd.split(' '), stdout=PIPE, stderr=PIPE)
+        (output, error) = proc.communicate()
+        output = str(output, 'utf-8')
+        return output.split('\t')[1].replace('\n','')
